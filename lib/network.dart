035360ae497f6afbc4fe2f5d85acd7273478692f
ndungudedan <dnkibere@gmail.com>
2022-08-25 14:17:03 +0300

    some changes

--- lib/network.dart
+++ lib/network.dart
@@ -1,99 +1,88 @@
 import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-const  FlutterSecureStorage  secureStorage = FlutterSecureStorage();
+const FlutterSecureStorage secureStorage = FlutterSecureStorage();
 
-class  ApiResponse {
-bool  success;
-String? errorMessage;
-ApiResponse({required  this.success, this.errorMessage});
+class ApiResponse {
+  bool success;
+  String? errorMessage;
+  ApiResponse({required this.success, this.errorMessage});
 }
 
-class  NetworkApi {
-
+class NetworkApi {
 // Use this when building for Windows
-static const FUSION_AUTH_DOMAIN = 'http://localhost:9011';
+  static const FUSION_AUTH_DOMAIN = 'http://localhost:9011';
 
 //Use this when building for Android
-static  const  FUSION_AUTH_DOMAIN = 'http://your-ip-address:9011';
+  static const FUSION_AUTH_DOMAIN = 'http://your-ip-address:9011';
 
-static  const  apiKey = 'your-API-key';
+  static const apiKey = 'your-API-key';
 
-static  const  applicationId = 'your-application-id';
+  static const applicationId = 'your-application-id';
 
-static  final  dio = Dio();  
+  static final dio = Dio();
 
-static  Future<ApiResponse> login(String  username, String  password) async {
-try {
-var  result = await  dio.post(
-'$FUSION_AUTH_DOMAIN/api/login',
-data: jsonEncode({
-"applicationId": applicationId,
-'loginId': username,
-'password': password,
-}),
-options: Options(headers: <String, String>{'Authorization': apiKey}),
-);
+  static Future<ApiResponse> login(String username, String password) async {
+    try {
+      var result = await dio.post(
+        '$FUSION_AUTH_DOMAIN/api/login',
+        data: jsonEncode({
+          "applicationId": applicationId,
+          'loginId': username,
+          'password': password,
+        }),
+        options: Options(headers: <String, String>{'Authorization': apiKey}),
+      );
 
-print(result.data);
+      print(result.data);
 
 //Securely Save important data such as token received from the server
-secureStorage.write(key: 'token', value: result.data['token']);
-secureStorage.write(key: 'tokenExpirationInstant', value: result.data['tokenExpirationInstant'].toString());
-secureStorage.write(key: 'username', value: result.data['user']['username']);
+      secureStorage.write(key: 'token', value: result.data['token']);
+      secureStorage.write(
+          key: 'tokenExpirationInstant',
+          value: result.data['tokenExpirationInstant'].toString());
+      secureStorage.write(
+          key: 'username', value: result.data['user']['username']);
 
-return  ApiResponse(success: true);
+      return ApiResponse(success: true);
+    } on DioError catch (e) {
+      print(e);
+      print(e.response);
+      return ApiResponse(success: false, errorMessage: "An Error occured");
+    } catch (e) {
+      print(e);
+      return ApiResponse(success: false, errorMessage: 'An Error Occurred');
+    }
+  }
 
-} on  DioError  catch (e) {
+  static Future<ApiResponse> register(String username, String password) async {
+    try {
+      var result = await dio.post(
+        '$FUSION_AUTH_DOMAIN/api/user/registration/',
+        data: jsonEncode({
+          "registration": {
+            "applicationId": applicationId,
+            "roles": [
+              "gamer",
+            ],
+          },
+          "user": {
+            'username': username,
+            'password': password,
+          },
+        }),
+        options: Options(headers: <String, String>{'Authorization': apiKey}),
+      );
 
-print(e);
-print(e.response);
-return  ApiResponse(success: false, errorMessage: "An Error occured");
-
-} catch (e) {
-
-print(e);
-return  ApiResponse(success: false, errorMessage: 'An Error Occurred');
-}
-}
-
-static  Future<ApiResponse> register(String  username, String  password) async {
-try {
-var  result = await  dio.post(
-'$FUSION_AUTH_DOMAIN/api/user/registration/',
-
-data: jsonEncode({
-"registration": {
-"applicationId": applicationId,
-"roles": [
-"gamer",
-],
-},
-"user": {
-'username': username,
-'password': password,
-},
-}),
-
-options: Options(headers: <String, String>{'Authorization': apiKey}),
-);
-
-return  ApiResponse(success: true);
-
-} on  DioError  catch (e) {
-
-print(e);
-print(e.response);
-return  ApiResponse(success: false, errorMessage: "An Error occured");
-
-} catch (e) {
-
-print(e);
-return  ApiResponse(success: false, errorMessage: 'An Error Occurred');
-
-}
-}
-
+      return ApiResponse(success: true);
+    } on DioError catch (e) {
+      print(e);
+      print(e.response);
+      return ApiResponse(success: false, errorMessage: "An Error occured");
+    } catch (e) {
+      print(e);
+      return ApiResponse(success: false, errorMessage: 'An Error Occurred');
+    }
+  }
 }
